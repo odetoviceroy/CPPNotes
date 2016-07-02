b9792821c3c7794af89cd2ac733c7f701ed7fd30
George Juarez <jjuarez@albany.edu>
2016-07-01 23:05:09 -0400

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,37 @@
+// George Juarez
+
+/*
+  Chapter 0:
+  - C++ has many fundamental facilities, such as IO, that are part of the standard library, rather
+  than part of the core language
+  - Programs that ask for standard-library facilities by using #include directives
+  - Output operator is << to write a string on the standard output, and then to write the value of std::endl
+  - Preceding a name by std:: indicated that the name is part of a namespace named std...std::cout
+  is the standard output stream
+   - Writing the value of std::endl ends the current line of output, so that if this program were to produce
+  any more output, that output would appear on a new line
+  - An expression asks the implementation to compute something...computation yields a result, and has side-effects
+  - << is left-associative, when << appears twice or more in the same expression, each << will use as much of the
+  expression as it can for its left operands, and as little of it as it can for its right operands
+  - Key property of manipulators is that writing a manipulator on a stream manipulates the stream, by doing something
+  other than just writing characters to it
+  - std::cout is a qualified name, which uses the :: operator, operator is known as the scope operator...to the left of
+  the :: is the name of the scope that is defined in the scope named on the left...this, std::cout means "the name cout
+  that is in the (namespace) scope std"
+  - Curly braces make the body of main and every function a scope in itself
+  - Quick review of special strings: \n (newline), \t (tab), \b (backspace), \" (treats this symbol as part of the
+  string terminator), \' (same meaning as ' in string literals, for consistency with character literals), \\ (includes
+  a \ in the string, treating the next character as an ordinary character
+  - C++ programs are usually in free form, spaces are required only when they keep adjacent symbols from running together
+  - types define data structures and operations on these data structures, C++ has two kinds: built-in (ex: int) and those
+  defined outside the core langauge (ex: std::ostream)
+  - Namespaces are a mechanism for grouping related names, names from the standard library are defined in the namespace as std
+
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+  cout << "Hello, world!" << endl;
+  return 0;
+}
